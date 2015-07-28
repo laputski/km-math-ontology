40a7d5ae79733c84ccaac213a25249f2c62383cd
PihtaHorse <mamul95@bk.ru>
2015-07-28 21:52:49 +0300

    Ещё немного layout

--- ontologySite/ontologySite/urls.py
+++ ontologySite/ontologySite/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.mainPage, name='mainPage'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^entitis/(?P<entity_id>[0-9]+)/$', views.entity, name='entity')
+    url(r'^entitis/(?P<entity_id>[0-9]+)/$', views.entity, name='entity'),
+    url(r'^entitis/(?P<entity_id>[0-9]+)/update$', views.entityUpdate, name='entityUpdate'),
+    url(r'^entitis/add/$', views.entityAdd, name='entityAdd'),
 ]
--- ontologySite/ontologySite/views.py
+++ ontologySite/ontologySite/views.py
@@ -5,12 +5,46 @@ from . import workWithNeo4j
 
 def mainPage(request):
     entity_list = workWithNeo4j.allEntities()
-    context = RequestContext(request, {'entity_list': entity_list})
+    
+    for entity in entity_list:
+        entity.childrens =  workWithNeo4j.getChildrens(entity.id)
+        entity.parents =  workWithNeo4j.getParents(entity.id)
+        if entity.type == 'Theorem':
+            entity.score = 1
+        elif entity.type == 'Definition':
+            entity.score = 5
+        else: entity.score = 0
+
+    context = RequestContext(request, {'entityList': entity_list})
     return render(request, 'ontologySite/startPage.html', context)
     
 def entity(request, entity_id):
+    entity_list = workWithNeo4j.allEntities()
+    
+    for entity in entity_list:
+        entity.childrens =  workWithNeo4j.getChildrens(entity.id)
+        entity.parents =  workWithNeo4j.getParents(entity.id)
+        if str(entity.id) == entity_id:
+            entity.score = 10
+        elif entity.type == 'Theorem':
+            entity.score = 1
+        elif entity.type == 'Definition':
+            entity.score = 5
+        else: entity.score = 0
+        
+    entity = workWithNeo4j.entityById(entity_id)
+    childrens = workWithNeo4j.getChildrens(entity_id)
+    parents = workWithNeo4j.getParents(entity_id)
+    context = RequestContext(request, {'entity': entity, 'childrens': childrens, 'parents': parents, 'entityList': entity_list})
+    return render(request, 'ontologySite/entity.html', context)
+    
+def entityUpdate(request, entity_id):
     entity = workWithNeo4j.entityById(entity_id)
     childrens = workWithNeo4j.getChildrens(entity_id)
     parents = workWithNeo4j.getParents(entity_id)
     context = RequestContext(request, {'entity': entity, 'childrens': childrens, 'parents': parents})
-    return render(request, 'ontologySite/entity.html', context)
+    return render(request, 'ontologySite/entityUpdate.html', context)
+    
+def entityAdd(request):
+    context = RequestContext(request, {})
+    return render(request, 'ontologySite/entityAdd.html', context)
