5435c2b696ac593ee207e41bd5ccbd0bf0c3c506
7kov <mockingbirdz@mail.ru>
2015-07-29 17:29:56 +0300

    Сайт с поиском.

--- ontologySite/ontologySite/urls.py
+++ ontologySite/ontologySite/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.mainPage, name='mainPage'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^entitis/(?P<entity_id>[0-9]+)/$', views.entity, name='entity')
+    url(r'^entitis/(?P<entity_id>[0-9]+)/$', views.entity, name='entity'),
+    url(r'^entitis/[0-9]+/search/$', views.search_titles, name='search_titles'),
+    url(r'^search/$', views.search_titles, name='search_titles'),
 ]
--- ontologySite/ontologySite/views.py
+++ ontologySite/ontologySite/views.py
@@ -1,16 +1,38 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from . import workWithNeo4j
+from .workWithNeo4j import Entity
+from django.core.context_processors import csrf
 
 def mainPage(request):
-    entity_list = workWithNeo4j.allEntities()
-    context = RequestContext(request, {'entity_list': entity_list})
-    return render(request, 'ontologySite/startPage.html', context)
+	args = {}
+	args.update(csrf(request))
+	args['entitis'] = workWithNeo4j.allEntities()
+	return render_to_response('ontologySite/startPage.html', args)
+
     
 def entity(request, entity_id):
-    entity = workWithNeo4j.entityById(entity_id)
-    childrens = workWithNeo4j.getChildrens(entity_id)
-    parents = workWithNeo4j.getParents(entity_id)
-    context = RequestContext(request, {'entity': entity, 'childrens': childrens, 'parents': parents})
-    return render(request, 'ontologySite/entity.html', context)
+
+	args = {}
+	args.update(csrf(request))
+	args['entity'] = workWithNeo4j.entityById(entity_id)
+	args['childrens'] = workWithNeo4j.getChildrens(entity_id)
+	args['parents'] = workWithNeo4j.getParents(entity_id)
+	return render_to_response('ontologySite/entity.html', args)
+
+def search_titles(request):
+	if request.method == "POST":
+		search_text = request.POST['search_text']
+	else:
+		search_text = ''
+
+	allentitis = workWithNeo4j.allEntities()
+	entitis = list()
+	for entity in allentitis:
+		if search_text in entity.name:
+			entitis.append( entity )
+
+	#entitis = workWithNeo4j.objects.filter(name__contains = search_text)
+
+	return render_to_response('ajax_search.html', {'entitis' : entitis, 'length': len(entitis)})
